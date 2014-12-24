3ab09209b5b44758403d757acef16f4a553caea8
Sergio Santoro <santoro.srg@gmail.com>
2014-12-24 16:25:10 +0100

    Add instructions for Composer

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-require './vendor/autoload.php';
-
+loadComposer();
 main();
 
-function main() {
+function main()
+{
     $app = new Application();
     $app->run();
+}
+
+function loadComposer()
+{
+    $autoloadPath = './vendor/autoload.php';
+    if (!file_exists($autoloadPath)) {
+        echo 'Questo programma usa Composer. Vedi https://getcomposer.org/ per maggiori dettagli.' . PHP_EOL;
+        exit(1);
+    }
+    require $autoloadPath;
 }
